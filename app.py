576222c32b7a89276bfc6cdd19b61a8534967f80
alyarama27 <oktaviaalya91.@gmail.com>
2025-05-02 11:43:03 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+#hitung luas segitiga sederhana
+def luas_segitiga():
+    a =int(input("masukin alas segitiga:"))
+    t =int(input("masukin tinggi segitiga:" ))
+    luas = a * t/2
+    print("luas segitiga adalah:", luas)
+
+luas_segitiga()
